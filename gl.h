2043c935425e80e1efb70c4e9aa942380bfd07a3
Alexander Rath <rath@sunstone.samba.cg.uni-saarland.de>
2020-05-18 09:54:11 +0200

    make gl.h work under Linux

--- gl.h
+++ gl.h
@@ -1,6 +1,7 @@
 #if defined(__APPLE__)
 #define GL_SILENCE_DEPRECATION
-#include <OpenGL/gl.h>
+//#include <OpenGL/gl.h>
+#include <OpenGL/gl3.h>
 #else
-#include <GL/gl.h>
+#include <GL/gl3w.h>
 #endif
